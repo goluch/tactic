91538aacaa5683f88bc5d5c7660f8b8484834062
goluch <goluch@eti.pg.edu.pl>
2022-04-06 20:18:04 +0200

    added NMKEngine test empty file

---
+++ UnitTestGameEngines/unittestNMKEngine.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <tchar.h>
+#include <iostream>
+#include "CppUnitTest.h"
+
+#define UNIT_TEST
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestGameEngines
+{
+	TEST_CLASS(UnitTestGameEngines)
+	{
+	public:
+
+		TEST_METHOD(TestNMKEngineMethodXXX)
+		{
+
+		}
+	};
+}
