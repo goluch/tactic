94ee542a31c8c6072d695fa42acfca044fd9fa9d
Tomasz <tomasz.goluch@gmail.com>
2023-05-15 12:07:15 +0200

    added comments of GameEngine abstract class

--- gameEngines/GameEngine.h
+++ gameEngines/GameEngine.h
@@ -13,12 +13,34 @@ namespace gameEngines
     class GameEngine
     {
     public:
-
+        /// <summary>
+        /// Returns true if the game is over, otherwise returns false
+        /// </summary>
+        /// <returns>Answer the question: if the game is over? True if yes, false if no.</returns>
         virtual bool IsGameOver() = 0;
-        virtual int Evaluate(Player) = 0;
-        virtual GameEngine* GeneratePossibleMoves(int&, Player) = 0;
+        /// <summary>
+        /// Evaluation of the game state from the player's point of view
+        /// </summary>
+        /// <param name="player">The player from whose point of view the evaluation is done</param>
+        /// <returns>Game state evaluation value from the selected player's point of view</returns>
+        virtual int Evaluate(Player player) = 0;
+        /// <summary>
+        /// Generates all possible moves without repetition.
+        /// </summary>
+        /// <param name="count">Number of returned possible game moves</param>
+        /// <param name="activePlayer">Information about the active player</param>
+        /// <returns>Table of possible moves in the game</returns>
+        virtual GameEngine* GeneratePossibleMoves(int& count, Player activePlayer) = 0;
+        /// <summary>
+        /// Gets the state of the game as a string
+        /// </summary>
+        /// <returns>String coding the state of the game</returns>
         virtual string GetGameState() = 0;
-        virtual void SetGameState(std::string) = 0;
+        /// <summary>
+        /// Sets the game state from a given string
+        /// </summary>
+        /// <param name="gameState">String coding the state of the game</param>
+        virtual void SetGameState(std::string gameState) = 0;
     };
 
 }
